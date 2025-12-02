7dcc2770ca654cdd742c8a6e4ba9f5dabec88293
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-12-02 16:07:57 +0100

    Complete allocation management
    Implement InitPcbs(), freePcb() and allocPcb(). Also add the auxiliary function initPcb()

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -4,41 +4,100 @@ static struct list_head pcbFree_h;
 static pcb_t pcbFree_table[MAXPROC];
 static int next_pid = 1;
 
-void initPcbs() {
+void initPcbs()
+{
+    INIT_LIST_HEAD(&pcbFree_h);
+
+    for (int i = 0; i < MAXPROC; i++)
+    {
+        list_add(&(pcbFree_table[i].p_list), &pcbFree_h);
+    }
 }
 
-void freePcb(pcb_t* p) {
+void freePcb(pcb_t *p)
+{
+    list_add(&(p->p_list), &pcbFree_h);
 }
 
-pcb_t* allocPcb() {
+// Reset all the values of the pcb
+void initPcb(pcb_t *p)
+{
+
+    INIT_LIST_HEAD(&p->p_list);
+    p->p_parent = NULL;
+    INIT_LIST_HEAD(&p->p_child);
+    INIT_LIST_HEAD(&p->p_sib);
+    p->p_s.entry_hi = 0;
+    p->p_s.cause = 0;
+    p->p_s.status = 0;
+    p->p_s.pc_epc = 0;
+    p->p_s.mie = 0;
+    for (int i = 0; i < STATE_GPR_LEN; i++)
+    {
+        p->p_s.gpr[i] = 0;
+    }
+    p->p_semAdd = NULL;
+    p->p_supportStruct = NULL;
+    p->p_time = 0;
+
+    p->p_prio = 0;
+    p->p_pid = next_pid;
+    next_pid++;
 }
 
-void mkEmptyProcQ(struct list_head* head) {
+pcb_t *allocPcb()
+{
+    struct list_head *list = list_next(&pcbFree_h);
+    if (list == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        list_del(list);
+    }
+
+    pcb_t *p = container_of(list, pcb_t, p_list);
+    initPcb(p);
+    return p;
 }
 
-int emptyProcQ(struct list_head* head) {
+void mkEmptyProcQ(struct list_head *head)
+{
 }
 
-void insertProcQ(struct list_head* head, pcb_t* p) {
+int emptyProcQ(struct list_head *head)
+{
 }
 
-pcb_t* headProcQ(struct list_head* head) {
+void insertProcQ(struct list_head *head, pcb_t *p)
+{
 }
 
-pcb_t* removeProcQ(struct list_head* head) {
+pcb_t *headProcQ(struct list_head *head)
+{
 }
 
-pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
+pcb_t *removeProcQ(struct list_head *head)
+{
 }
 
-int emptyChild(pcb_t* p) {
+pcb_t *outProcQ(struct list_head *head, pcb_t *p)
+{
 }
 
-void insertChild(pcb_t* prnt, pcb_t* p) {
+int emptyChild(pcb_t *p)
+{
 }
 
-pcb_t* removeChild(pcb_t* p) {
+void insertChild(pcb_t *prnt, pcb_t *p)
+{
 }
 
-pcb_t* outChild(pcb_t* p) {
+pcb_t *removeChild(pcb_t *p)
+{
+}
+
+pcb_t *outChild(pcb_t *p)
+{
 }
