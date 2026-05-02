158c16c20c91928a7d3c380c5319a9e673e12488
mazzettimatteo <mazzetti.matteo.ita@gmail.com>
2026-05-02 11:58:48 +0200

    Add: pager procedure

--- phase3/src/vmSupport.c
+++ phase3/src/vmSupport.c
@@ -1,15 +1,18 @@
 
 #include "../../headers/const.h"    
-#include <uriscv/liburiscv.h>       
+#include <uriscv/liburiscv.h>
+#include <uriscv/cpu.h>       
 
 #include "../headers/vmSupport.h"
 #include "../../headers/types.h"
 #include "../../headers/const.h"
 #include "../../phase2/src/initial.c"
+#include "../headers/sysSupport.h"
 
 static swap_t swapPool[POOLSIZE];
 int swapSemaphore;
 extern pcb_t *currProc;
+extern 
 
 void initPagetable(pteEntry_t *pageTable, int asid) {
     for (int i = 0; i < MAXPAGES-1; i++) {
@@ -42,6 +45,78 @@ void initSwapPool() {
     swapSemaphore = 1;
 }
 
+// SEZIONE 4.2 - Mazzo
+
+void pager() {
+    
+    support_t *suppPtr = (support_t *)SYSCALL(GETSUPPORTPTR, 0, 0, 0); //Forse non serve casting a (support_t *)
+
+    
+    int cause = suppPtr->sup_exceptState[0].cause;
+    cause=(cause & GETEXECCODE) >> CAUSESHIFT;
+
+    
+    if(cause==EXC_MOD) programTrapHandler(suppPtr);
+
+    
+    SYSCALL(PASSEREN, &swapSemaphore, 0, 0);
+
+    
+    int missingPageNo = suppPtr->sup_exceptState[0].entry_hi;
+    missingPageNo = missingPageNo & GETPAGENO;
+
+    static int fifoCont=0;
+    int i=fifoCont;
+    fifoCont=(fifoCont+1) % POOLSIZE;
+
+    int physicalFrame=FLASHPOOLSTART +(i*PAGESIZE);
+
+    
+    if(swapPool[i].sw_asid!=-1) {
+        //Frame occupied
+        int x=swapPool[i].sw_asid;
+        int k=swapPool[i].sw_pageNo;
+        pteEntry_t *xPte=swapPool[i].sw_pte;
+
+        xPte->pte_entryLO &= ~VALIDON;
+
+        int status=getSTATUS();
+        setSTATUS(status & ~MSTATUS_MIE_MASK);
+        TLBCLR();
+        setSTATUS(status);
+
+        readWriteFlashdrive(x,k,physicalFrame,FLASHWRITE);
+
+    }
+
+    readWriteFlashdrive(suppPtr->sup_asid, missingPageNo, physicalFrame, FLASHREAD);
+
+    swapPool[i].sw_asid=suppPtr->sup_asid;
+    swapPool[i].sw_pageNo=missingPageNo;
+
+    int pageIdx;
+    if(missingPageNo==0xBFFFF) //Entry 31: pagina stack (VPN 0xBFFFF)
+        pageIdx=31;
+    else
+        pageIdx=missingPageNo % MAXPAGES;
+
+    swapPool[i].sw_pte=&suppPtr->sup_privatePgTbl[pageIdx];
+
+    swapPool[i].sw_pte->pte_entryLO=physicalFrame | DIRTYON | VALIDON;
+
+    int status=getSTATUS();
+    setSTATUS(status & ~MSTATUS_MIE_MASK);
+    TLBCLR();
+    setSTATUS(status);
+
+    SYSCALL(VERHOGEN,&swapSemaphore,0,0);
+
+    LDST(&suppPtr->sup_exceptState[0]);
+}
+
+//SEZIONE 5.1 - Mazzo
+
+
 // funzione aux per ricavare il blocco dato il VPN - Virtual Page Number
 // blocco 31 <=> vpn 0xBFFFF
 // blocchi[0..30] <=> vpn 0x80000,0x80001,..,0x8001E
@@ -62,9 +137,6 @@ devreg_t * getFlashRegister(int asid){
     
     return( (devreg_t *)devRegister );
 }
-
-
-//SEZIONE 5.1 - Mazzo
 
 /* da documentazione:
 
