1c49cb9188df4638e065c8aaa7d41df66cdb3869
mazzettimatteo <mazzetti.matteo.ita@gmail.com>
2026-05-02 12:24:52 +0200

    Fix: moved section 4.2 after 5.1

--- phase3/headers/vmSupport.h
+++ phase3/headers/vmSupport.h
@@ -1,1 +1,3 @@
 void uTLB_RefillHandler(void);
+void pager(void);
+int readWriteFlashdrive(int asid, int vpn, int phisicalFrame, int op);
--- phase3/src/vmSupport.c
+++ phase3/src/vmSupport.c
@@ -45,6 +45,57 @@ void initSwapPool() {
     swapSemaphore = 1;
 }
 
+//SEZIONE 5.1 - Mazzo
+
+// funzione aux per ricavare il blocco dato il VPN - Virtual Page Number
+// blocco 31 <=> vpn 0xBFFFF
+// blocchi[0..30] <=> vpn 0x80000,0x80001,..,0x8001E
+// quindi per vpn=0x80002 il blocco è il 2
+int getFlashBlock(int vpn){
+    if(vpn==0xBFFFF) return 31; 
+    else return(vpn-0x80000);
+}
+
+// funzione aux per calcolare l'addr base dei registri dei flash dev
+// ogni processo utente ha un suo flash dev il cui asid va da 1..8, 
+// segue che i registi vanno da 0..7 = (1..8)-1
+devreg_t * getFlashRegister(int asid){
+    int devNo=asid-1;
+
+    //segue tabella per gestione interrupt implementata in phase2
+    int devRegister=START_DEVREG+((IL_FLASH-3)*0x80)+(devNo*0x10); 
+    
+    return( (devreg_t *)devRegister );
+}
+
+/* da documentazione:
+
+(A) Write the flash device’s DATA0 field with the appropriate starting physical address of the 4k
+block to be read (or written); the particular frame’s starting address.
+(B) Use the NSYS5 system call to write the flash device’s COMMAND field with the device block
+number (high order three bytes) and the command to read (or write) in the lower order byte.
+Where the following C code can be used to request a NSYS5:
+int ioStatus = SYSCALL(DOIO, int *commandAddr, int commandValue, 0);
+Where the mnemonic constant DOIO has the value of -5.
+
+*/
+int readWriteFlashdrive(int asid, int vpn, int phisicalFrame, int op){
+
+    int block=getFlashBlock(vpn);
+    devreg_t * reg=getFlashRegister(asid);
+
+    reg->dtp.data0=phisicalFrame; //(A)
+
+    // let's shift "block" up by 8bits(his size) so "op" can occupy the 4th byte
+    // we'll end up un a situation like the following: [0..0][0..0][block][op]
+    int command= (block<<8) | op; 
+
+    int ioStatus;
+    ioStatus=SYSCALL(DOIO, (int)&(reg->dtp.command),command,0); //(B)
+    
+    //credo ci vada qualcos'altro se status!=1 ossia status!=READY
+}
+
 // SEZIONE 4.2 - Mazzo
 
 void pager() {
@@ -131,54 +182,3 @@ void pager() {
     LDST(&suppPtr->sup_exceptState[0]);
 }
 
-//SEZIONE 5.1 - Mazzo
-
-
-// funzione aux per ricavare il blocco dato il VPN - Virtual Page Number
-// blocco 31 <=> vpn 0xBFFFF
-// blocchi[0..30] <=> vpn 0x80000,0x80001,..,0x8001E
-// quindi per vpn=0x80002 il blocco è il 2
-int getFlashBlock(int vpn){
-    if(vpn==0xBFFFF) return 31; 
-    else return(vpn-0x80000);
-}
-
-// funzione aux per calcolare l'addr base dei registri dei flash dev
-// ogni processo utente ha un suo flash dev il cui asid va da 1..8, 
-// segue che i registi vanno da 0..7 = (1..8)-1
-devreg_t * getFlashRegister(int asid){
-    int devNo=asid-1;
-
-    //segue tabella per gestione interrupt implementata in phase2
-    int devRegister=START_DEVREG+((IL_FLASH-3)*0x80)+(devNo*0x10); 
-    
-    return( (devreg_t *)devRegister );
-}
-
-/* da documentazione:
-
-(A) Write the flash device’s DATA0 field with the appropriate starting physical address of the 4k
-block to be read (or written); the particular frame’s starting address.
-(B) Use the NSYS5 system call to write the flash device’s COMMAND field with the device block
-number (high order three bytes) and the command to read (or write) in the lower order byte.
-Where the following C code can be used to request a NSYS5:
-int ioStatus = SYSCALL(DOIO, int *commandAddr, int commandValue, 0);
-Where the mnemonic constant DOIO has the value of -5.
-
-*/
-int readWriteFlashdrive(int asid, int vpn, int phisicalFrame, int op){
-
-    int block=getFlashBlock(vpn);
-    devreg_t * reg=getFlashRegister(asid);
-
-    reg->dtp.data0=phisicalFrame; //(A)
-
-    // let's shift "block" up by 8bits(his size) so "op" can occupy the 4th byte
-    // we'll end up un a situation like the following: [0..0][0..0][block][op]
-    int command= (block<<8) | op; 
-
-    int ioStatus;
-    ioStatus=SYSCALL(DOIO, (int)&(reg->dtp.command),command,0); //(B)
-    
-    //credo ci vada qualcos'altro se status!=1 ossia status!=READY
-}
