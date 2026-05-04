1b2a6744d823a7d0f430f37a76a07c5ce67ca2b3
milino05 <milodisalvatore6@gmail.com>
2026-05-04 20:55:24 +0200

    shell and partial calc

--- phase3/testers/calc.c
+++ phase3/testers/calc.c
@@ -1,7 +1,34 @@
 #include <uriscv/liburiscv.h>
 
-#include "h/tconst.h"
 #include "h/print.h"
+#include "h/tconst.h"
+
+char *deleteSpaces(char *expr, int len, char *newExpr) {
+    for (int i = 0; i < len; i++) {
+        if (expr[i] == ' ') {
+            continue;
+        }
+        newExpr[i] = expr[i];
+    }
+}
+
+int asciiToInt(char *expr, int first, char opr, int second, int len) {
+    char *newExpr;
+    deleteSpaces(expr, len, newExpr);
+    first = newExpr[0] - 48;
+    if (newExpr[1] == '+' || newExpr[1] == '-' || newExpr[1] == '*' ||
+        newExpr[1] == '/') {
+        opr = newExpr[1];
+    }
+    second = newExpr[2];
+}
+
+int calculate(int first, char opr, int second) {}
 
 void main() {
+    char buffer[6], operand;
+    int first, second;
+    int n = SYSCALL(READTERMINAL, (int)buffer, 0, 0);
+    asciiToInt(buffer, first, operand, second, n);
+    int res = first
 }
--- phase3/testers/shell.c
+++ phase3/testers/shell.c
@@ -1,7 +1,58 @@
 #include <uriscv/liburiscv.h>
 
-#include "h/tconst.h"
 #include "h/print.h"
+#include "h/tconst.h"
+
+void chkExitAndTerminate(char *buff) {
+    if (buff[0] == 'e' && buff[1] == 'x' && buff[2] == 'i' && buff[3] == 't' &&
+        buff[4] == '\0') {
+        SYSCALL(TERMINATE, 0, 0, 0);
+    }
+}
 
 void main() {
+
+    struct mapAsid {
+        char *name;
+        int asid;
+    };
+    struct mapAsid arr[] = {
+        {"calc", 2}, {"miao", 3}, {"meow", 4},
+        {"meow", 5}, {"meow", 6}, {"meow", 7},
+    };
+    char *startPrompt = "> ";
+    char *unknownProg = "command not found :<\n";
+    char buffer[80];
+    int found, excAsid;
+    while (1) {
+        SYSCALL(WRITETERMINAL, (int)startPrompt, 2, 0);
+        // n = number of characters actually read
+        int n = SYSCALL(READTERMINAL, (int)buffer, 0, 0);
+        if (buffer[n - 1] == '\n') {
+            buffer[n - 1] = '\0';
+        }
+
+        chkExitAndTerminate(buffer);
+
+        found = 0;
+        for (int i = 0; i < 6; i++) { // search in known program asid's
+            int index = 0;
+
+            while (arr[i].name[index] == buffer[index]) {
+                if (arr[i].name[index] == '\0') {
+                    found = 1;
+                    excAsid = arr[i].asid;
+                    break;
+                }
+                index++;
+            }
+            if (found == 1) {
+                SYSCALL(EXECUTE, excAsid, 0, 0);
+                break;
+            }
+        }
+        if (found == 0) {
+            SYSCALL(WRITETERMINAL, (int)unknownProg, 21, 0);
+        }
+    }
 }
