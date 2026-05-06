99411b9f260d01c9c20b91deba9093302db5a464
milino05 <milodisalvatore6@gmail.com>
2026-05-06 12:06:25 +0200

    setTimer fixed in scheduler too

--- phase2/src/scheduler.c
+++ phase2/src/scheduler.c
@@ -1,36 +1,34 @@
-#include "../headers/initial.h"
 #include "../headers/scheduler.h"
-#include "../../headers/types.h"
 #include "../../headers/const.h"
 #include "../../headers/listx.h"
-#include <uriscv/liburiscv.h>
+#include "../../headers/types.h"
 #include "../../phase1/headers/pcb.h"
+#include "../headers/initial.h"
+#include <uriscv/liburiscv.h>
 
 extern struct list_head readyQueue;
 extern int processCount;
 extern int softBlock_count;
-extern pcb_t* currProc;
+extern pcb_t *currProc;
 extern cpu_t tod_start;
 
-void dispatch(){
-    while(emptyProcQ(&readyQueue)){
-        if (processCount == 0){ 
+void dispatch() {
+    while (emptyProcQ(&readyQueue)) {
+        if (processCount == 0) {
             HALT();
-        }
-        else if(softBlock_count > 0){ //There are no ready process but some are waiting
+        } else if (softBlock_count > 0) { // There are no ready process but some are waiting
             setMIE(MIE_ALL & ~MIE_MTIE_MASK);
             unsigned int status = getSTATUS();
             status |= MSTATUS_MIE_MASK;
             setSTATUS(status);
             WAIT();
-        }
-        else{ //Fallback case when a deadlock is happening
+        } else { // Fallback case when a deadlock is happening
             PANIC();
         }
     }
-    pcb_t* pcb = removeProcQ(&readyQueue);
+    pcb_t *pcb = removeProcQ(&readyQueue);
     currProc = pcb;
-    setTIMER(TIMESLICE); //Load 5 milliseconds on the PLT
+    setTIMER(TIMESLICE * *((int *)TIMESCALEADDR)); // Let 5ms pass on the processor
     STCK(tod_start);
-    LDST(&(pcb->p_s)); //Perform a Load Processor State on the processor state stored in PCB of the Current Process of the current CPU
+    LDST(&(pcb->p_s)); // Perform a Load Processor State on the processor state stored in PCB of the Current Process of the current CPU
 }
